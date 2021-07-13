2dcaefd518cc3c63f7bb5760f307b00b9a2d15db
LalithaSistla <80061996+LalithaSistla@users.noreply.github.com>
2021-07-13 21:37:36 +0530

    Update Form1.cs
    Latest Changes - Added DataList to read the input files and fill the data

--- TestCommand/TestCommand/Form1.cs
+++ TestCommand/TestCommand/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,28 +25,58 @@ namespace TestCommand
             
         }
 
+        public struct StructClaim
+        {
+            public string REC_TYPE;
+            public string BPID_TYPE;
+            public string RECIPIENT;
+            public string DOC_TEMPLATE;           
+        }
         private void btnProcess_Click(object sender, EventArgs e)
         {
-            string strInputFile,strRecFile,strLine = null;
-            string strcode = null;
+            string strDataFile,strCodeFile,strLine = null;
+            string strREC_TYPE, str_BPID_TYPE = null;
             FileStream oInputFileStream = null;
             StreamReader oInputFileReader = null;
+            Dictionary<string, Tuple<string, string>> oDict = null;
+            Tuple<string, string> oTuple;
             try
             {
-                strInputFile = txtBoxInputFile.Text;
-                strRecFile = txtBoxRecFile.Text;
-                if (System.IO.File.Exists(strInputFile))
+                strDataFile = txtBoxInputFile.Text; 
+                strCodeFile = txtBoxRecFile.Text;
+                
+                if (System.IO.File.Exists(strCodeFile))
                 {
-
-                    oInputFileStream = new FileStream(strInputFile, FileMode.Open, FileAccess.Read);
+                    oDict = ReadDataFromCodeFile(strCodeFile);
+                }
+                
+                   
+                
+                if (System.IO.File.Exists(strDataFile))
+                {
+                    
+                    oInputFileStream = new FileStream(strDataFile, FileMode.Open, FileAccess.Read);
                     oInputFileReader = new StreamReader(oInputFileStream);
+                    StructClaim structClaim = new StructClaim();
+                    ArrayList oDataList = new ArrayList();
+                    
                     while (!oInputFileReader.EndOfStream)
                     {
                         strLine = oInputFileReader.ReadLine();
-                        strcode = strLine.Substring(16, 4);
-                        if(string.Compare(strcode,"BPID")==0)
+                        strREC_TYPE = strLine.Substring(16, 4);                        
+                        if(string.Compare(strREC_TYPE,"BPID")==0)
                         {
-                            string str1 = strLine.Substring(40, 4);
+                             str_BPID_TYPE = strLine.Substring(40, 4);
+                            if (oDict != null)
+                            {
+                                 oTuple = oDict[str_BPID_TYPE];
+                                structClaim.REC_TYPE = strREC_TYPE;
+                                structClaim.BPID_TYPE = str_BPID_TYPE;
+                                structClaim.RECIPIENT = oTuple.Item1;
+                                structClaim.DOC_TEMPLATE = oTuple.Item2;
+                                oDataList.Add(structClaim);
+
+                            }
 
                         }
                     }
@@ -62,17 +93,33 @@ namespace TestCommand
 
         }
 
-
-        private void GetRecData(string skey)
+        private Dictionary<string,Tuple<string,string>> ReadDataFromCodeFile(string strFileName)
         {
+            string strLine = null;
+            FileStream oInputFileStream = null;
+            StreamReader oInputFileReader = null;
+            Dictionary<string, Tuple<string, string>> oDict;
+            string[] strArray;
             try
             {
+                oInputFileStream = new FileStream(strFileName, FileMode.Open, FileAccess.Read);
+                oInputFileReader = new StreamReader(oInputFileStream);
+                
+                oDict = new Dictionary<string, Tuple<string, string>>();
+                while (!oInputFileReader.EndOfStream)
+                {
+                    strLine = oInputFileReader.ReadLine();
+                    strArray = strLine.Split(',');
+                    oDict.Add(strArray[0], new Tuple<string, string>(strArray[1], strArray[2]));
 
+                }
+                return oDict;
             }
-            catch(Exception)
+            catch(Exception ex)
             {
-
+                return null;
             }
         }
+       
     }
 }
